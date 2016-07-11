3d445f00ab414c24e6c47fb69a53e5b548a2caf1
Eegras <eegras@eegrasstudios.com>
2016-07-11 13:24:32 -0500

    Changes to not interfere with /u/PCMRBot

--- botlearn.py
+++ botlearn.py
@@ -2,6 +2,8 @@ import praw
 import time
 import random
 import obot
+# Regular expression support
+import re
 
 # oAuth
 
@@ -13,11 +15,11 @@ def login():
 
 r = login()
 
-
 # oAuth end
 
 steam_games = ['[Grand Theft Auto 5](http://store.steampowered.com/app/271590/)', '[No Man\'s sky](http://store.steampowered.com/app/275850/)', '[Doom](http://store.steampowered.com/app/379720/)', '[ARK: Survival Evolved](http://store.steampowered.com/app/346110/)', '[Rocket League](http://store.steampowered.com/app/252950/)']
-words_to_match = ['link me a good game', '!game', 'what is a good game', 'what\'s a good game', 'link me a game']
+# These are just straight contains matching.  We want to do a bit more under certain circumstances
+words_to_match = ['link me a good game', 'what is a good game', 'what\'s a good game', 'link me a game']
 
 cache = []
 
@@ -32,8 +34,13 @@ def run_bot():
     for comment in comments:
         comment_text = comment.body.lower()
         isMatch = any(string in comment_text for string in words_to_match)
+
+        # Regular expression matching.
+        isRegex = True
+        if re.compile('^!game$', re.MULTILINE).search(comment_text) == None:
+            isRegex = False
         
-        if comment.id not in cache and isMatch:
+        if comment.id not in cache and ( isMatch or isRegex ):
             comment.reply("{} is a great game!\n\n -----  \n\n  This action was done automatically, if you have any questions or concerns, contact the creator: /u/amanat361".format(random.choice(steam_games)))     
             
             print("Mission Accomplished")
